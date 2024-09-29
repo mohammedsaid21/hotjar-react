709a5f9ab54f8375746cbf730fd2172b73b6f250
Ahmed Mansour <ahmed@loom.li>
2024-09-29 05:39:21 -0400

    layout: Scroll-bar shift bad fix
    The scroll-bar caused a layout shift between pages that had a scroll bar
    and pages that did not. This was nasty and super noticeable because of
    the margin lines component. This change updates the global.css file to
    get rid of the scrollbar entirely for now. This may not be the approved
    solution but it will do for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import "./globals.css";
 import Navbar from "@/components/sections/navbar";
 import Footer from "@/components/sections/footer";
 import MarginLines from "@/components/ui/margin-lines";
-// import { Script } from 'next/script';
 import Script from 'next/script';
 import GTM from "@/components/gtm";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -29,28 +29,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <head>
-        {/* <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "o9o1c4jahg");
-          `}
-        </Script> */}
         <GTM />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-background text-foreground`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-full bg-background text-foreground overflow-x-hidden`}
       >
         <Navbar />
-        <MarginLines>
-          <main className="flex flex-col flex-grow max-w-7xl mx-auto px-4 py-8">
-            {children}
-          </main>
-        </MarginLines>
+        <div className="flex flex-col flex-grow">
+          <MarginLines>
+            <main className="flex-grow">{children}</main>
+          </MarginLines>
+        </div>
         <Footer />
 
         <Script id="microsoft-clarity" strategy="afterInteractive">
